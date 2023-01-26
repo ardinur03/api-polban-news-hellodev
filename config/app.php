44701f759a0a2278e393e38f2edf88f8d7f4cab6
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2023-01-26 20:27:36 +0700

    FEAT : authentification with SPA Breeze

--- config/app.php
+++ config/app.php
@@ -185,7 +185,8 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Barryvdh\Debugbar\ServiceProvider::class,
+        
         /*
          * Application Service Providers...
          */
