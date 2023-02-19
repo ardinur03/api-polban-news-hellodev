eb0af1a27f13a5607ac79ce7ec6a885a840ec6be
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2023-02-19 10:28:19 +0700

    FIX : fix api improve search and filter feature on news API

--- app/Http/Controllers/API/NewController.php
+++ app/Http/Controllers/API/NewController.php
@@ -18,24 +18,30 @@ class NewController extends Controller
         $title = $request->input('title');
         $slug = $request->input('slug');
         $content = $request->input('content');
+        $date_filter = $request->input('date_filter');
+        $scope = $request->input('scope');
 
-        $news = DB::table('news')
-            ->rightJoin('student_center_news', 'news.id', '=', 'student_center_news.new_id')
-            ->leftJoin('campus_organizations', 'student_center_news.campus_organization_code', '=', 'campus_organizations.code_campus_organization')
-            ->select('news.*', 'student_center_news.campus_organization_code AS code')
-            ->where('news.status', '=', 'published')
-            ->where('news.deleted_at', '=', null)
-            ->union(
-                DB::table('news')
-                    ->rightJoin('student_association_news', 'news.id', '=', 'student_association_news.new_id')
-                    ->where('news.deleted_at', '=', null)
-                    ->where('news.status', '=', 'published')
-                    ->select('news.*', 'student_association_news.faculty_organizations_code AS code')
-            );
+        $news = DB::table(function ($query) {
+            $query->select('news.*', 'student_center_news.campus_organization_code AS code')
+                ->selectRaw("CASE WHEN student_center_news.campus_organization_code IS NOT NULL THEN 'pusat' ELSE 'himpunan' END AS scope")
+                ->from('news')
+                ->rightJoin('student_center_news', 'news.id', '=', 'student_center_news.new_id')
+                ->leftJoin('campus_organizations', 'student_center_news.campus_organization_code', '=', 'campus_organizations.code_campus_organization')
+                ->where('news.status', '=', 'published')
+                ->whereNull('news.deleted_at')
+                ->union(
+                    DB::table('news')
+                        ->rightJoin('student_association_news', 'news.id', '=', 'student_association_news.new_id')
+                        ->whereNull('news.deleted_at')
+                        ->where('news.status', '=', 'published')
+                        ->select('news.*', 'student_association_news.faculty_organizations_code AS code')
+                        ->selectRaw("'himpunan' AS scope")
+                );
+        });
+
 
         if ($id) {
             $news_first =  $news->where('news.id', '=', $id);
-
             if ($news_first) {
                 return ResponseFormatter::success(
                     $news_first->first(),
@@ -60,6 +66,41 @@ class NewController extends Controller
 
         if ($content) {
             $news->where('news.content', 'like', '%' . $content . '%');
+        }
+
+        if ($date_filter) {
+            if ($date_filter == 'latest') {
+                $news->orderBy('created_at', 'DESC');
+            } else if ($date_filter == 'oldest') {
+                $news->orderBy('created_at', 'ASC');
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Date filter tidak ada',
+                    404
+                );
+            }
+        }
+
+        if ($limit == 0) {
+            return ResponseFormatter::success(
+                NewsResource::collection($news->get()),
+                'Data list news berhasil diambil'
+            );
+        }
+
+        if ($scope) {
+            if ($scope == 'pusat') {
+                $news->where('scope', '=', 'pusat');
+            } else if ($scope == 'himpunan') {
+                $news->where('scope', '=', 'himpunan');
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Scope tidak ada',
+                    404
+                );
+            }
         }
 
         return ResponseFormatter::success(
