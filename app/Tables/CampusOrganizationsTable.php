8d8067dc128ba376b1a061d61155f568eb8c1cff
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2023-05-07 23:24:53 +0700

    FIX: Fixed proxy server configuration

--- app/Tables/CampusOrganizationsTable.php
+++ app/Tables/CampusOrganizationsTable.php
@@ -4,8 +4,10 @@ namespace App\Tables;
 
 use App\Models\CampusOrganization;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use ProtoneMedia\Splade\AbstractTable;
 use ProtoneMedia\Splade\SpladeTable;
+use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class CampusOrganizationsTable extends AbstractTable
@@ -37,8 +39,15 @@ class CampusOrganizationsTable extends AbstractTable
      */
     public function for()
     {
-        $campus_organizations = CampusOrganization::orderBy('code_campus_organization', 'desc');
-        return QueryBuilder::for($campus_organizations)->allowedSorts(['code_campus_organization', 'name_campus_organization']);
+        $globalSearch = AllowedFilter::callback('global', function ($query, $value) {
+            $query->where(function ($query) use ($value) {
+                Collection::wrap($value)->each(function ($value) use ($query) {
+                    $query->orWhere('name_campus_organization', 'LIKE', "%{$value}%")->orWhere('code_campus_organization', 'LIKE', "%{$value}%");
+                });
+            });
+        });
+        $campus_organizations = CampusOrganization::orderBy('code_campus_organization', 'asc');
+        return QueryBuilder::for($campus_organizations)->allowedSorts(['code_campus_organization', 'name_campus_organization'])->allowedFilters([$globalSearch]);
     }
 
     /**
@@ -49,6 +58,6 @@ class CampusOrganizationsTable extends AbstractTable
      */
     public function configure(SpladeTable $table)
     {
-        $table->column(key: 'code_campus_organization', sortable: true)->column(key: 'name_campus_organization', sortable: true)->column('action')->paginate(10);
+        $table->withGlobalSearch()->column(key: 'code_campus_organization', sortable: true)->column(key: 'name_campus_organization', sortable: true)->column('action')->paginate(10);
     }
 }
